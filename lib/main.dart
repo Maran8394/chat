11ec5501656846f9ce5997ae906fefa626f32c7b
Maran8394 <maran8394@gmail.com>
2022-07-08 08:08:32 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:chat/screens/welcome_screen.dart';
+import 'package:chat/screens/login_screen.dart';
+import 'package:chat/screens/registration_screen.dart';
+import 'package:chat/screens/chat_screen.dart';
+import 'package:device_preview/device_preview.dart';
+
+// void main() => runApp(const FlashChat());
+void main() => runApp(
+      DevicePreview(
+        enabled: true,
+        builder: (context) => FlashChat(), // Wrap your app
+      ),
+    );
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        theme: ThemeData.dark().copyWith(
+          textTheme: const TextTheme(
+            bodyText1: TextStyle(color: Colors.black54),
+          ),
+        ),
+        initialRoute: WelcomeScreen.id,
+        routes: {
+          WelcomeScreen.id: (context) => WelcomeScreen(),
+          LoginScreen.id: (context) => LoginScreen(),
+          RegistrationScreen.id: (context) => RegistrationScreen(),
+          ChatScreen.id: (context) => ChatScreen(),
+        });
+  }
+}
